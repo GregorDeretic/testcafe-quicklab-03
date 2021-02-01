5f840227fc73fd02b04a2a19212363d13ebe898e
Gregor Deretic <gregor.deretic@sky.uk>
2021-02-01 14:37:06 +0000

    Heroku app added

---
+++ test/heroku_test.ts
@@ -0,0 +1,20 @@
+import {Selector} from "testcafe";
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+ }
+
+fixture `Simple test`            // Notice the back-tick, NOT a single or double quote
+    .page `https://the-internet.herokuapp.com/`;
+
+    test('form authentication', async t => {
+
+        await t
+            .click(`a[href="https://the-internet.herokuapp.com/login"]`)
+            .typeText(`#username`, `tomsmith`)
+            .typeText(`#password`, `SuperSecretpassword!`)
+            .expect(Selector(`a`))
+
+        await sleep(5000);
+
+    });
